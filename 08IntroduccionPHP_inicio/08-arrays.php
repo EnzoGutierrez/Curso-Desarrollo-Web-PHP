3f217c166b92f83c3b590caef48ed367b1780f89
EnzoGutierrez <gutierrezenzo911@gmail.com>
2022-08-20 10:20:08 -0300

    Update 08-arrays.php

--- 08IntroduccionPHP_inicio/08-arrays.php
+++ 08IntroduccionPHP_inicio/08-arrays.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
+$carrito = ["tablet", "Television", "Computadora"];
 
+//Añadir un elemento nuevo al final
+array_push($carrito, "Audifonos");
+
+//Acceder a un elemento del arrray
+echo $carrito[1];
+
+//Añadir al inicio
+array_unshift($carrito, "Monitor");
+
+//Util para ver contenidos de un array
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
 
 
 include 'includes/footer.php';
